cbb93835e5eae5f327d240190221b2c4460098d3
wasnikruchika701-dotcom <wasnikruchika701@gmail.com>
2025-11-07 18:35:17 +0530

    First commit

---
+++ a1.py
@@ -0,0 +1,5 @@
+# 1. list
+# function in list- len() ,min(), max(),type()
+ 
+colors =['red ','green' ,'white' , 'pink ', 'yellow ' ]
+num =[23,44,65,78,90,443]
---
+++ py3
@@ -0,0 +1,3 @@
+red = 12
+area = 3.14 * red * red
+print("Area of circle = ",area)
